6af5935b50011b9bc1413bc0eb40e8aefe3f704a
Ahmad AlAhmad <41687535+Pungent2001@users.noreply.github.com>
2024-02-28 20:09:15 +0300

    Added API communication + password validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -11,13 +13,16 @@ class MyApp extends StatelessWidget {
       title: 'kfupm chat',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch()
-            .copyWith(secondary: Color.fromRGBO(85, 138, 94, 1)),
+            .copyWith(secondary: const Color.fromRGBO(85, 138, 94, 1)),
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor:
-            Color.fromRGBO(85, 138, 94, 1), // RGB(85,138,94) for background
-        textTheme: TextTheme(
+        scaffoldBackgroundColor: const Color.fromRGBO(
+            85, 138, 94, 1), // RGB(85,138,94) for background
+        textTheme: const TextTheme(
+          // ignore: deprecated_member_use
           bodyText1: TextStyle(color: Colors.white), // White text color
+          // ignore: deprecated_member_use
           bodyText2: TextStyle(color: Colors.white), // White text color
+          // ignore: deprecated_member_use
           button: TextStyle(
               color: Color.fromRGBO(
                   85, 138, 94, 1)), // RGB(85,138,94) for button text
@@ -29,11 +34,27 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  Future<void> fetchData() async {
+    final url =
+        'https://fa9e-2001-16a2-c0ba-36fa-1698-4f09-6d44-15f8.ngrok-free.app/api/';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the JSON
+      final jsonData = jsonDecode(response.body);
+      print('JSON Data: $jsonData');
+      // Handle the JSON data as needed
+    } else {
+      // If the server returns an error response, throw an exception
+      throw Exception('Failed to load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: const Text('Home'),
       ),
       body: Center(
         child: Column(
@@ -43,41 +64,47 @@ class HomePage extends StatelessWidget {
               'https://upload.wikimedia.org/wikipedia/ar/archive/3/37/20180719130502%21King_Fahd_University_of_Petroleum_%26_Minerals_Logo.png',
               height: 150,
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white, // White button color
-                    foregroundColor: Color.fromRGBO(
-                        85, 138, 94, 1), // RGB(85,138,94) for button text
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => RegistrationPage()),
-                    );
-                  },
-                  child: Text('Register'),
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white, // White button color
-                    foregroundColor: Color.fromRGBO(
-                        85, 138, 94, 1), // RGB(85,138,94) for button text
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignInPage()),
-                    );
-                  },
-                  child: Text('Sign In'),
-                ),
-              ],
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                fetchData();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white, // White button color
+                foregroundColor: const Color.fromRGBO(
+                    85, 138, 94, 1), // RGB(85,138,94) for button text
+              ),
+              child: const Text('Fetch Data'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistrationPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white, // White button color
+                foregroundColor: const Color.fromRGBO(
+                    85, 138, 94, 1), // RGB(85,138,94) for button text
+              ),
+              child: const Text('Register'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignInPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white, // White button color
+                foregroundColor: const Color.fromRGBO(
+                    85, 138, 94, 1), // RGB(85,138,94) for button text
+              ),
+              child: const Text('Sign In'),
             ),
           ],
         ),
@@ -86,72 +113,108 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+  bool _registerButtonEnabled = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Registration'),
+        title: const Text('Registration'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Registration Page',
+            const Text('Registration Page',
                 style: TextStyle(color: Colors.white)), // White text color
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(8.0),
               child: TextField(
+                controller: _emailController,
                 decoration: InputDecoration(
                   labelText: 'Email',
-                  labelStyle:
-                      TextStyle(color: Colors.white), // White text color
+                  labelStyle: TextStyle(color: Colors.white),
                 ),
+                onChanged: (_) => _updateRegisterButton(),
               ),
             ),
-            SizedBox(height: 10),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(8.0),
               child: TextField(
+                controller: _passwordController,
                 decoration: InputDecoration(
                   labelText: 'Password',
-                  labelStyle:
-                      TextStyle(color: Colors.white), // White text color
+                  labelStyle: TextStyle(color: Colors.white),
                 ),
                 obscureText: true,
+                onChanged: (_) => _updateRegisterButton(),
               ),
             ),
-            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _confirmPasswordController,
+                decoration: InputDecoration(
+                  labelText: 'Confirm Password',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
+                obscureText: true,
+                onChanged: (_) => _updateRegisterButton(),
+              ),
+            ),
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Show registration success dialog
-                showDialog(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                    title: Text('Success'),
-                    content: Text('You have been successfully registered!'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context); // Close dialog
-                          Navigator.pop(context); // Go back to home page
-                        },
-                        child: Text('OK'),
-                      ),
-                    ],
-                  ),
-                );
-              },
+              onPressed: _registerButtonEnabled ? _register : null,
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white, // White button color
-                foregroundColor: Color.fromRGBO(
+                backgroundColor: _registerButtonEnabled
+                    ? Colors.white
+                    : Colors.grey, // White button color
+                foregroundColor: const Color.fromRGBO(
                     85, 138, 94, 1), // RGB(85,138,94) for button text
               ),
-              child: Text('Register'),
+              child: const Text('Register'),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  void _updateRegisterButton() {
+    setState(() {
+      _registerButtonEnabled = _emailController.text.isNotEmpty &&
+          _passwordController.text.isNotEmpty &&
+          _confirmPasswordController.text.isNotEmpty &&
+          _passwordController.text == _confirmPasswordController.text;
+    });
+  }
+
+  void _register() {
+    // Perform registration logic here
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Success'),
+        content: const Text('Registration successful!'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // Close dialog
+              Navigator.pop(context); // Go back to home page
+            },
+            child: const Text('OK'),
+          ),
+        ],
       ),
     );
   }
@@ -162,51 +225,48 @@ class SignInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign In'),
+        title: const Text('Sign In'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Sign In Page',
+            const Text('Sign In Page',
                 style: TextStyle(color: Colors.white)), // White text color
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(8.0),
               child: TextField(
                 decoration: InputDecoration(
                   labelText: 'Email',
-                  labelStyle:
-                      TextStyle(color: Colors.white), // White text color
+                  labelStyle: TextStyle(color: Colors.white),
                 ),
               ),
             ),
-            SizedBox(height: 10),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(8.0),
               child: TextField(
                 decoration: InputDecoration(
                   labelText: 'Password',
-                  labelStyle:
-                      TextStyle(color: Colors.white), // White text color
+                  labelStyle: TextStyle(color: Colors.white),
                 ),
                 obscureText: true,
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => ChatPage()),
+                  MaterialPageRoute(builder: (context) => ConversationsPage()),
                 );
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.white, // White button color
-                foregroundColor: Color.fromRGBO(
+                foregroundColor: const Color.fromRGBO(
                     85, 138, 94, 1), // RGB(85,138,94) for button text
               ),
-              child: Text('Continue'),
+              child: const Text('Sign In'),
             ),
           ],
         ),
@@ -215,15 +275,15 @@ class SignInPage extends StatelessWidget {
   }
 }
 
-class ChatPage extends StatelessWidget {
+class ConversationsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chats'),
+        title: const Text('Conversations'),
       ),
-      body: Center(
-        child: Text('Chat Page (Placeholder)',
+      body: const Center(
+        child: Text('Conversations Page (Placeholder)',
             style: TextStyle(color: Colors.white)), // White text color
       ),
     );
